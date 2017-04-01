91f2122176cd9b00f943055b1786bae687356b5e
shaddu <shadab.ahmed63@gmail.com>
2017-04-02 01:54:54 +0530

    print ll

--- Linked List/python/LinkedList.py
+++ Linked List/python/LinkedList.py
@@ -2,24 +2,34 @@ class Node(object):
       def __init__(self, _data = None, _next = None):
         self.data = _data
         self.next= _next
+        
+      def get_data(self):
+          return self.data
+
+      def get_next(self):
+        return self.next_node
+
+      def set_next(self, new_next):
+        self.next_node = new_next
+
 
 class LinkedList (object):
-    head = Node(None)
-    def __init__( self ):
-        head = None
+    def __init__( self, head=None):
+        self.head = head
     
     def push(self, key):
         node = Node(key)
-        node.next = self.head
-        head = node
+        node.set_next(self.head)
+        self.head = node
     
     def __str__(self):
         temp = self.head
         output = ""
         while temp:
-            output = str(temp.data) + "->"
-            temp = temp.next
-        return output
+            output = output + str(temp.get_data()) + "->"
+            temp = temp.get_next()
+        
+        return output[:-2]
 
 
 
