b7cde296479a6ddf29a4e9683dd7f47cbb09af45
shaddu <shadab.ahmed63@gmail.com>
2017-04-10 00:00:43 +0530

    commenting

--- Numbers/fast-gcd.cpp
+++ Numbers/fast-gcd.cpp
@@ -1,91 +1,75 @@
+/*This program was written as solution for hackerearth coding challenge */
+
 #include <iostream>
-#include <vector>
-#include <list>
+#include <algorithm>
+#include <cmath>
+
 using namespace std;
-
-class Graph
+//fast gcd finder , For Details : https://github.com/lemire/Code-used-on-Daniel-Lemire-s-blog/blob/77e5335ef0091426aeee10a6780740626ae118a8/2013/12/26/gcd.cpp
+long gcd(long u, long v)
 {
-	int V;
-	int count=0;
-	list<int> *adj;
-		public :
-	Graph(int v)
-	{
-		V=v;
-		adj = new list<int>[V];
-	};
-	void dfs(int s,bool visited[])
-	{
-			//cout<<s<<" ";
-			visited[s]=true;
-			count++;
-				for(list<int>::iterator it=adj[s].begin();it!=adj[s].end();++it)
-					{
-						if(!visited[*it])
-							dfs(*it,visited);
-					}
- 
-	};
- 
-	int check(int s)
-	{
-			bool visited[V];
-		for(int i=0;i<V;i++)
-			visited[i]=false;
-		dfs(s, visited);
-		return count;
-		
-	};
-	
-	void edge(int u,int v)
-	{
-		adj[u].push_back(v);
-	};
-	
-	bool edgecheck(int u , int v)
-	{	
-			for(list<int>::iterator it=adj[v].begin();it!=adj[v].end();++it)
-					{
-						if(*it == u)
-						return true;
-					}
-		return false;
-	}
+	int shift, uz, vz;
+     if ( u == 0) return v;
+     if ( v == 0) return u;
+     uz = __builtin_ctz(u);
+     vz = __builtin_ctz(v);
+     shift = uz > vz ? vz : uz;
+     u >>= uz;
+     do {
+       v >>= vz;
+       int diff = v;
+       diff -= u;
+       vz = __builtin_ctz(diff);
+       if ( diff == 0 ) break;
+       if ( v <  u ) 
+         u = v;
+       v = abs(diff);
 
-};
+     } while( 1 );
+     return u << shift;
+}
+//Fast poweroftwo finder , Details : http://www.exploringbinary.com/ten-ways-to-check-if-an-integer-is-a-power-of-two-in-c/
+bool isPowerOfTwo(int x)
+{	return x && (!(x&(x-1))); }
 
-int main() {
-	// your code goes 
-	long T,N,M,u,v;
-	bool loop = false;
-	cin>>T;
-	vector<pair<int,int>> prereq;
-	while(T--)
-	{
-		loop = false;
-		cin>>N>>M;
-		Graph g(N+1);
-		for(int i = 0;i<M;i++)
-			{
-				cin>>u>>v;
-				g.edge(u,v);
-				if (g.edgecheck(u,v))
-				{
-					cout<<"0\n";
-					loop = true;
-					break;
-				}
-				
-				//prereq.push_back(make_pair(u,v));
-			//	check(N,prereq);
-			}
-		if(g.check(1)==N  )
-		{
-			if(!loop)
-			cout<<"1\n";
-		}
-		else
-			cout<<"0\n";
-	}
+int fun(long N , int PIN)
+{
+	if(N%PIN != 0)
 	return 0;
+	
+	long M ;
+	M = N/PIN;
+	
+	int value = 0;
+	
+	for(long i = 1;i<=M;i++)
+	{
+		if(__gcd(i,M)==i)
+			value++;
+	}
+	if(isPowerOfTwo(value))
+		return value;
+	else
+		return 0;
+	
+}
+
+int main()
+{
+  //To Do : Fast IO	
+  long N,T;
+  cin>>T;
+  while(T--)
+  {
+  	cin>>N;
+  	long temp, max=0;
+  	for(int i =1 ;i<=13;i++)
+  		{
+			temp = fun(N,i);  
+			if(temp > max)
+  				max = temp;
+		  }
+  	cout<<max<<"\n";
+  }
+  return 0;
 }
