83cb5db092199d6bc53a41456bdbdc112d502962
shaddu <shadab.ahmed63@gmail.com>
2017-04-22 22:39:49 +0530

    sub functions

--- Stack/towerofhanoi.py
+++ Stack/towerofhanoi.py
@@ -6,9 +6,9 @@ def TowerOfHanoi(disk_count):
     pole_source = stack()
     pole_destination = stack()
     pole_aux = stack()
-    for i in range(0,disk_count - 1):
+    for i in range(0,disk_count):
         pole_source.push(disk_count - i)
-    moves = pow(2,disk_count) - 1
+    moves = pow(2,disk_count) 
     if disk_count%2 == 0:
         MakeLegalMoves(moves,pole_source, pole_destination, pole_aux)
     else:
@@ -21,32 +21,21 @@ def TowerOfHanoi(disk_count):
 def MakeLegalMoves(count, stack_A, stack_B, stack_C):
     for i in range(1,count):
         if i%3 == 1:
-            if stack_C.isEmpty():
-                 if not stack_A.isEmpty():
-                     stack_C.push(stack_A.pop())
-            else:
-                if stack_A.peek() < stack_C.peek():
-                    stack_C.push(stack_A.pop())
-                else:
-                    stack_C.push(stack_A.pop())
+            MoveDisk(stack_C, stack_A)
         if i%3 == 2:
-            if stack_B.isEmpty():
-                 if not stack_A.isEmpty():
-                     stack_B.push(stack_A.pop())
-            else:
-                if stack_A.peek() < stack_B.peek():
-                    stack_B.push(stack_A.pop())
-                else:
-                    stack_A.push(stack_B.pop())
+            MoveDisk(stack_B, stack_A)
         if i%3 == 0:
-            if stack_C.isEmpty():
-                 if not stack_B.isEmpty():
-                     stack_C.push(stack_B.pop())
-            else:
-                if stack_B.isEmpty():
-                    stack_B.push(stack_C.pop())
-                elif stack_B.peek() < stack_C.peek():
-                    stack_C.push(stack_B.pop())
-                else:
-                    stack_B.push(stack_C.pop())
-     
+            MoveDisk(stack_C, stack_B)
+
+def MoveDisk(stack_X, stack_Y):
+    if stack_X.isEmpty():
+        if not stack_Y.isEmpty():
+            stack_X.push(stack_Y.pop())
+    else:
+        if stack_Y.isEmpty():
+            stack_Y.push(stack_X.pop())
+        elif stack_Y.peek() < stack_X.peek():
+            stack_X.push(stack_Y.pop())
+        else:
+            stack_Y.push(stack_X.pop())
+
