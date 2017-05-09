dd0e283a461654e5b4ff96f4ac7d91e12f6e0a83
shaddu <shadab.ahmed63@gmail.com>
2017-05-09 23:57:25 +0530

    nose testing

--- Search/main.py
+++ Search/main.py
@@ -1,8 +1,10 @@
 from binarysearch import binarysearch
+from nose.tools import assert_equal, assert_raises
 
 def binarysearch_test():
     arr = [1, 2, 3, 4, 8, 9]
-    print binarysearch(arr, 4)
+    print "checking binary search"
+    assert_equal(binarysearch(arr, 4),3)
 
 
 if __name__ == "__main__":
