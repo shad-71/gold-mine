d685c6adea87ba299170c53fe4f3db0a096d083b
shaddu <shadab.ahmed63@gmail.com>
2017-05-12 23:15:32 +0530

    sorting

--- Search/main.py
+++ Search/main.py
@@ -4,10 +4,9 @@ from nose.tools import assert_equal, assert_raises
 def binarysearch_test():
     arr = [1, 2, 3, 4, 8, 9]
     print "checking binary search"
-    assert_equal(binarysearch(arr, 4),3)
+    assert_equal(binarysearch(arr, 4), 3)
     print "success"
 
 
 if __name__ == "__main__":
-    #test_1()
     binarysearch_test()
