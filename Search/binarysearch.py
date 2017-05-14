c81c75304d11700729ffe60a108efca618b9ed2a
shaddu <shadab.ahmed63@gmail.com>
2017-05-14 18:20:05 +0530

    floor & ciel

--- Search/binarysearch.py
+++ Search/binarysearch.py
@@ -2,11 +2,42 @@ def binarysearch(arr, x) :
     l = 0
     r = len(arr)
     while r - l >=0:
-        mid = l + (r - l)/2
+        mid = l + (r - l)/2 #help in overflow problem
         if x == arr[mid]:
             return mid
         elif x < arr[mid]:
             r = mid
         else:
             l = mid
+
+def floor(arr, x):
+    l = -1
+    r = len(arr) - 1
+    while r - l > 1:
+        mid = l + ( r - l)/2
+        if arr[mid] >= x:
+            r = mid
+        else:
+            l = mid
+    if arr[r] == x:
+        return r
+    else:
+        return -1
+
+def ciel(arr, x):
+    l = 0
+    r = len(arr)
+    while r - l > 1:
+        mid = l + ( r - l)/2
+        if arr[mid] <= x:
+            l = mid
+        else:
+            r = mid
+    if arr[l] == x:
+        return l
+    else:
+        return -1
+
+def occurence_count(arr, x):
+    return ciel(arr, x) - floor(arr, x) + 1
             
--- Search/main.py
+++ Search/main.py
@@ -1,4 +1,4 @@
-from binarysearch import binarysearch
+from binarysearch import *
 from nose.tools import assert_equal, assert_raises
 
 def binarysearch_test():
@@ -7,6 +7,14 @@ def binarysearch_test():
     assert_equal(binarysearch(arr, 4), 3)
     print "success"
 
+def occurence_count_test():
+    arr = [1, 1, 1, 2, 2, 2, 2, 3, 3, 4, 4, 4, 4]
+    print "occurence test"
+    assert_equal(occurence_count(arr, 2), 4)
+    assert_equal(occurence_count(arr, 1), 3)
+    assert_equal(occurence_count(arr, 4), 4)
+    print "success"
 
 if __name__ == "__main__":
-    binarysearch_test()
+    # binarysearch_test()
+    occurence_count_test()
